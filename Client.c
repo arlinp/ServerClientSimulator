40d99ad12fa67d34e8b4c0f4c0037f621445e3fc
arlin <arlin>
2019-10-12 14:54:07 -0600

    commit

--- Client.c
+++ Client.c
@@ -93,35 +93,71 @@ void func(int sockfd)
   
 int TCP(int port) 
 { 
-    int sockfd, connfd; 
-    struct sockaddr_in servaddr, cli; 
+    /* Variable Definition */
+	int sockfd; 
+	int nsockfd;
+	char revbuf[LENGTH]; 
+	struct sockaddr_in remote_addr;
+
+	/* Get the Socket file descriptor */
+	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+	{
+	  fprintf(stderr, "ERROR: Failed to obtain Socket Descriptor! (errno = %d)\n",errno);
+	  exit(1);
+	}
+    
+    /* Fill the socket address struct */
+	remote_addr.sin_family = AF_INET; 
+	remote_addr.sin_port = htons(PORT); 
+	inet_pton(AF_INET, "127.0.0.1", &remote_addr.sin_addr); 
+	bzero(&(remote_addr.sin_zero), 8);
   
-    // socket create and varification 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0); 
-    if (sockfd == -1) { 
-        printf("socket creation failed...\n"); 
-        exit(0); 
-    } 
-    else
-        printf("Socket successfully created..\n"); 
-    bzero(&servaddr, sizeof(servaddr)); 
+    /* Try to connect the remote */
+	if (connect(sockfd, (struct sockaddr *)&remote_addr, sizeof(struct sockaddr)) == -1)
+	{
+	    error("ERROR: Failed to connect to the host!\n");
+	}
   
-    // assign IP, PORT 
-    servaddr.sin_family = AF_INET; 
-    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1"); 
-    servaddr.sin_port = htons(PORT); 
-  
-    // connect the client socket to server socket 
-    if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) != 0) { 
-        printf("connection with the server failed...\n"); 
-        exit(0); 
-    } 
-    else
-        printf("connected to the server..\n"); 
-  
-    // function for chat 
-    func(sockfd); 
-  
-    // close the socket 
-    close(sockfd); 
+	else
+	  printf("[Client] Connected to server at port %d...ok!\n", PORT);
+
+	* Receive File from Server */
+	printf("[Client] Receiveing file from Server and saving it as final.txt...");
+	char* fr_name = "/home/aryan/Desktop/progetto/final.txt";
+	FILE *fr = fopen(fr_name, "a");
+	if(fr == NULL)
+		printf("File %s Cannot be opened.\n", fr_name);
+	else
+	{
+	    bzero(revbuf, LENGTH); 
+	    int fr_block_sz = 0;
+	    int success = 0;
+	    //while(success == 0)
+	    //{
+	        while(fr_block_sz = recv(sockfd, revbuf, LENGTH, 0))
+	        {
+	            if(fr_block_sz < 0)
+	            {
+	                error("Receive file error.\n");
+	            }
+	            int write_sz = fwrite(revbuf, sizeof(char), fr_block_sz, fr);
+	            if(write_sz < fr_block_sz)
+	            {
+	                error("File write failed.\n");
+	            }
+				else if(fr_block_sz)
+				{
+					break;
+				}
+	            bzero(revbuf, LENGTH);
+	        }
+	        printf("Ok received from server!\n");
+	        success = 1;
+	        fclose(fr);
+	    //}
+	}
+	close (sockfd);
+	printf("[Client] Connection lost.\n");
+	return (0);
+        
 }
