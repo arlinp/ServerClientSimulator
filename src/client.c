95a00e2ed9d4ccaa68cb4da2d974ed50fcc3c3de
lpascoe <lpascoe@unm.edu>
2019-10-12 16:18:16 -0600

    Updated file read.

--- src/client.c
+++ src/client.c
@@ -41,7 +41,7 @@ int UDP(int port) {
     struct sockaddr_in     servaddr; 
   
     // Creating socket file descriptor 
-    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
         perror("socket creation failed"); 
         exit(EXIT_FAILURE); 
     } 
@@ -60,7 +60,7 @@ int UDP(int port) {
             sizeof(servaddr)); 
     printf("Hello message sent.\n"); 
           
-    n = recvfrom(sockfd, (char* )buffer, MAXLINE,  
+    n = recvfrom(sockfd, (char*)buffer, MAXLINE,  
                 MSG_WAITALL, (struct sockaddr*) &servaddr, 
                 &len); 
     buffer[n] = '\0'; 
@@ -102,7 +102,7 @@ int TCP(int port)
 	/* Get the Socket file descriptor */
 	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
 	{
-	  fprintf(stderr, "ERROR: Failed to obtain Socket Descriptor! (errno = %d)\n",errno);
+	  fprintf(stderr, "ERROR: Failed to obtain Socket Descriptor. (errno = %d)\n",errno);
 	  exit(1);
 	}
     
@@ -113,51 +113,43 @@ int TCP(int port)
 	bzero(&(remote_addr.sin_zero), 8);
   
     /* Try to connect the remote */
-	if (connect(sockfd, (struct sockaddr *)&remote_addr, sizeof(struct sockaddr)) == -1)
-	{
-	    error("ERROR: Failed to connect to the host!\n");
+	if (connect(sockfd, (struct sockaddr *)&remote_addr, sizeof(struct sockaddr)) == -1){
+	    error("ERROR: Failed to connect to the host.\n");
 	}
-  
 	else
-	  printf("[Client] Connected to server at port %d...ok!\n", PORT);
+	  printf("Client connected to server at port %d...\n", PORT);
 
-	* Receive File from Server */
-	printf("[Client] Receiveing file from Server and saving it as final.txt...");
-	char* fr_name = "/home/aryan/Desktop/progetto/final.txt";
-	FILE *fr = fopen(fr_name, "a");
-	if(fr == NULL)
-		printf("File %s Cannot be opened.\n", fr_name);
-	else
-	{
+/*
+	printf("Client receiving file from Server...");
+	FILE *fr = fopen("./lab2.html", "a");
+	if{
 	    bzero(revbuf, LENGTH); 
 	    int fr_block_sz = 0;
 	    int success = 0;
 	    //while(success == 0)
 	    //{
-	        while(fr_block_sz = recv(sockfd, revbuf, LENGTH, 0))
-	        {
-	            if(fr_block_sz < 0)
-	            {
+	        while(fr_block_sz = recv(sockfd, revbuf, LENGTH, 0)){
+	            if(fr_block_sz < 0){
 	                error("Receive file error.\n");
 	            }
 	            int write_sz = fwrite(revbuf, sizeof(char), fr_block_sz, fr);
-	            if(write_sz < fr_block_sz)
-	            {
+	            if(write_sz < fr_block_sz){
 	                error("File write failed.\n");
 	            }
-				else if(fr_block_sz)
-				{
+				else if(fr_block_sz){
 					break;
 				}
 	            bzero(revbuf, LENGTH);
 	        }
-	        printf("Ok received from server!\n");
+	        printf("Ok!\n");
 	        success = 1;
 	        fclose(fr);
 	    //}
 	}
+    else(fr == NULL) printf("404 Not Found.\n");
+    */
 	close (sockfd);
-	printf("[Client] Connection lost.\n");
+	printf("Client connection lost.\n");
 	return (0);
         
 }
