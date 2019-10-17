a802be99dc111792599d9ab345cb5b00c1bb4dce
rfresquez <rfresquez@unm.edu>
2019-10-17 16:14:11 -0600

    fixing udp

--- src/webserver.c
+++ src/webserver.c
@@ -36,7 +36,69 @@ int main(int argc, char *argv[])
   }
 }
 
-int UDP(int port) { 
+int UDP(int port) {
+
+  /*int sockfd, nBytes; 
+    struct sockaddr_in addr_con; 
+    int addrlen = sizeof(addr_con); 
+    addr_con.sin_family = AF_INET; 
+    addr_con.sin_port = htons(PORT_NO); 
+    addr_con.sin_addr.s_addr = INADDR_ANY; 
+    char net_buf[NET_BUF_SIZE]; 
+    FILE* fp; 
+  
+    // socket() 
+    sockfd = socket(AF_INET, SOCK_DGRAM, IP_PROTOCOL); 
+  
+    if (sockfd < 0) 
+        printf("\nfile descriptor not received!!\n"); 
+    else
+        printf("\nfile descriptor %d received\n", sockfd); 
+  
+    // bind() 
+    if (bind(sockfd, (struct sockaddr*)&addr_con, sizeof(addr_con)) == 0) 
+        printf("\nSuccessfully binded!\n"); 
+    else
+        printf("\nBinding Failed!\n"); 
+  
+    while (1) { 
+        printf("\nWaiting for file name...\n"); 
+  
+        // receive file name 
+        clearBuf(net_buf); 
+  
+        nBytes = recvfrom(sockfd, net_buf, 
+                          NET_BUF_SIZE, sendrecvflag, 
+                          (struct sockaddr*)&addr_con, &addrlen); 
+  
+        fp = fopen(net_buf, "r"); 
+        printf("\nFile Name Received: %s\n", net_buf); 
+        if (fp == NULL) 
+            printf("\nFile open failed!\n"); 
+        else
+            printf("\nFile Successfully opened!\n"); 
+  
+        while (1) { 
+  
+            // process 
+            if (sendFile(fp, net_buf, NET_BUF_SIZE)) { 
+                sendto(sockfd, net_buf, NET_BUF_SIZE, 
+                       sendrecvflag,  
+                    (struct sockaddr*)&addr_con, addrlen); 
+                break; 
+            } 
+  
+            // send 
+            sendto(sockfd, net_buf, NET_BUF_SIZE, 
+                   sendrecvflag, 
+                (struct sockaddr*)&addr_con, addrlen); 
+            clearBuf(net_buf); 
+        } 
+        if (fp != NULL) 
+            fclose(fp); 
+    } 
+    return 0; */
+  
   int sockfd;
   char buffer[LENGTH]; 
   char *hello = "Hello from server"; 
